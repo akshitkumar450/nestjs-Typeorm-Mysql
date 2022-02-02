616445dea50b359d18b64b0b921bdee9e740e80c
Akshit Kumar <akshitkumar450@gmail.com>
2022-02-03 01:07:58 +0530

    connecting with the mysql database with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    // connecting to DB
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      database: 'carsdb',
+      // having all the entities (Schemas) for our app
+      entities: [User],
+      username: 'root',
+      password: '97177akshit',
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
