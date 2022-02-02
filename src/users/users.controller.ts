72ba13b88f1335f8cb1fedf3bf6a0eb9cf3db648
Akshit Kumar <akshitkumar450@gmail.com>
2022-02-03 01:57:20 +0530

    saving data to db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './DTO/create-user-dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  // DI
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    return 'created';
+    // console.log(body);
+    return this.usersService.createUser(body.email, body.password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //   userRepo: Repository<User>;
+  //   constructor(usersRepo: Repository<User>) {
+  //     this.userRepo = usersRepo;
+  //   }
+  //   OR
+  //   using typreorm repository
+  //   to interact with db we will use userRepo bcz it is an instance of Reporsitory of User
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  createUser(email: string, password: string) {
+    const user = this.userRepo.create({ email, password });
+    return this.userRepo.save(user);
+  }
+}
