a01111e3bdf343a6d774871f70d9bd450904fda7
Akshit Kumar <akshitkumar450@gmail.com>
2022-02-04 20:02:18 +0530

    showing limited fields in the response

---
+++ src/reports/DTO/report.dto.ts
@@ -0,0 +1,33 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  milaage: string;
+
+  // add new property to show
+  // obj is the original report entity
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
 import { CreateReportDto } from './DTO/create-report.dto';
+import { ReportDto } from './DTO/report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
@@ -10,6 +12,7 @@ export class ReportsController {
   // to use report service class methods
   constructor(private reportsService: ReportsService) {}
 
+  @Serialize(ReportDto) // to show only these properties
   @UseGuards(AuthGuard) //for only authenticated user can access the routes
   @Post('/')
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
